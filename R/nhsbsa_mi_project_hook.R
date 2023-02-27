d27ec5a7f96bd9aa44925a214d7c95c6b026e671
Mark McPherson <mark.mcpherson1@nhs.net>
2023-02-27 07:42:23 +0000

    Sort out dependencies and test

--- R/nhsbsa_mi_project_hook.R
+++ R/nhsbsa_mi_project_hook.R
@@ -10,23 +10,25 @@
 #' @param package_name Name of the package (aka the app or dashboard)
 #' @param ... Never used
 #'
+#' @import R.utils
+#' @import fs
+#' @import withr
+#'
 #' @export
-nhsbsa_mi_project_hook <- function(path, package_name, test_arg, ...) {
+nhsbsa_mi_project_hook <- function(path, package_name, ...) {
   # Delete all the files...
-  # browser()
-  # fs::dir_delete("dev")
-  # fs::dir_delete("inst")
-  # fs::dir_delete("man")
-  # fs::dir_delete("R")
-  # fs::file_delete(".Rbuildignore")
-  # fs::file_delete(".gitignore")
-  # fs::file_delete("NAMESPACE")
+  fs::dir_delete("dev")
+  fs::dir_delete("inst")
+  fs::dir_delete("man")
+  fs::dir_delete("R")
+  fs::file_delete(".Rbuildignore")
+  fs::file_delete(".gitignore")
+  fs::file_delete("NAMESPACE")
   # Don't delete the DESCRIPTION or .Rproj to prevent error message (tho message
   # can be safely ignored)
   # fs::file_delete("DESCRIPTION") # Exclude Linting
   # fs::file_delete(paste0(package_name, ".Rproj")) # Exclude Linting
 
   # Copy all contents of base_project dir into new project folder
-  # R.utils::copyDirectory(system.file("templates", package = "projecthooks"), ".")
-  write(test_arg, "test_file.txt")
+  R.utils::copyDirectory(system.file("mi_dash", "templates", package = "projecthooks"), ".")
 }
