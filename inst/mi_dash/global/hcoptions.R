b2cc034e52dbcddfc0eb4d3bc345007d11122b03
Mark McPherson <mark.mcpherson1@nhs.net>
2023-03-10 15:32:39 +0000

    Fully working

--- inst/mi_dash/global/hcoptions.R
+++
@@ -1,5 +0,0 @@
-### set options for highcharter package
-hcoptslang <- getOption("highcharter.lang")
-hcoptslang$thousandsSep <- ","
-hcoptslang$numericSymbols <- c("k", "M", "B", "T", "P", "E")
-options(highcharter.lang = hcoptslang)
